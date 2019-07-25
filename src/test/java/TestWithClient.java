d4c560cbdbf568ba4b3d9f295ba6a20f556df23d
Mateusz Kierznowski <mateusz.kierznowski@nask.pl>
2019-07-25 15:22:29 +0200

    inital commit

---
+++ src/test/java/TestWithClient.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class TestWithClient {
+//    AddEmp <EmpID> "<nazwisko>" "<adres>" H <stawka-godzinowa>
+//    AddEmp <EmpID> "<nazwisko>" "<adres>" S <wynagrodzenie-miesięczne>
+//    AddEmp <EmpID> "<nazwisko>" "<adres>" C <wynagrodzenie-miesięczne> <stawka-prowizji>
+
+    @Test
+    public void addEmpStawkaGodzinowa(){
+        EmployeeService service;
+        int id;
+        String lastName;
+        String name;
+        String adres;
+        service.add();
+    }
+
+
+}
